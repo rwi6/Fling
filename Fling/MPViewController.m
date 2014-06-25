41501e79e21cc3a16682b4496292b48d336cc183
Ritchie Iu <rwi6@cornell.edu>
2014-06-25 00:32:18 -0700

    Can send POST request to server to create new users.

--- Fling/MPViewController.m
+++ Fling/MPViewController.m
@@ -7,17 +7,28 @@
 //
 
 #import "MPViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface MPViewController ()
+@interface MPViewController () <CLLocationManagerDelegate>
 
+@property (weak, nonatomic) IBOutlet UILabel *latitude;
+@property (weak, nonatomic) IBOutlet UILabel *longitude;
+- (IBAction)jsonButtonPressed:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *address;
+- (IBAction)buttonPressed:(id)sender;
 @end
 
 @implementation MPViewController
+    CLLocationManager *manager;
+    CLGeocoder *geocoder;
+    CLPlacemark *placemark;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    manager = [[CLLocationManager alloc] init];
+    geocoder = [[CLGeocoder alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +37,89 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonPressed:(id)sender {
+    manager.delegate =  self;
+    manager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    [manager startUpdatingLocation];
+    
+}
+
+#pragma mark CLLocationManagerDelegate methods
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"Error: %@", error);
+    NSLog(@"Failed to get location!");
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
+    NSLog(@"Location: %@", newLocation);
+    CLLocation *currentLocation = newLocation;
+    
+    if (currentLocation != nil) {
+        self.latitude.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
+        self.longitude.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
+    }
+    
+    [geocoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray *placemarks, NSError *error) {
+        if (error == nil && [placemarks count] > 0) {
+            placemark = [placemarks lastObject];
+            self.address.text = [NSString stringWithFormat:@"%@ %@\n%@ %@\n%@\n%@",
+                                 placemark.subThoroughfare,
+                                 placemark.thoroughfare,
+                                 placemark.postalCode,
+                                 placemark.locality,
+                                 placemark.administrativeArea,
+                                  placemark.country];
+                                 
+        }
+        else {
+            NSLog(@"%@", error.debugDescription);
+        }
+    }];
+    
+    [manager stopUpdatingLocation];
+
+}
+- (IBAction)jsonButtonPressed:(id)sender {
+    NSLog(@"HI You pressed the button!");
+    NSDictionary *inner = @{
+      @"name": @"riu",
+      @"email": @"myEmail",
+      @"longitude":@"6",
+      @"latitude":@"7"};
+    
+    NSDictionary *dictionary = @{@"user": inner};
+    
+    NSData *JSONData = [NSJSONSerialization dataWithJSONObject:dictionary
+                                                       options:0 error:nil];
+    //NSString *someString = [NSString stringWithFormat:@"%@", JSONData];
+    
+    //NSString *JSONString = @"{'user': {'name': 'test2', 'email':'email2','longitude':'5','latitude':'21'}}";
+    //NSData *data = [JSONString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    sessionConfiguration.HTTPAdditionalHeaders = @{
+                                                   @"Content-Type"  : @"application/json"
+                                                   };
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration];
+
+    NSURL *url = [NSURL URLWithString:@"http://arcane-dawn-1935.herokuapp.com/users"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    request.HTTPBody = JSONData;
+    
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                                                 completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
+    {
+        if (!error) {
+            NSLog(@"Status code: %i", ((NSHTTPURLResponse *)response).statusCode);
+        }
+        else {
+            NSLog(@"Error: %@", error.localizedDescription);
+        }
+        
+    }];
+    [task resume];
+    //NSLog(someString);
+}
 @end
